5472393e6c1431b84588ffe554a8f60bb1ced1cb
Yossely Mendoza <yossely7@gmail.com>
2023-02-09 20:49:48 -0500

    refactor: convert race-result-list into smart component

--- src/app/pages/races/race-results-list/race-results-list.component.ts
+++ src/app/pages/races/race-results-list/race-results-list.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { RacesFinalResult } from 'src/app/+state/races/races.models';
+import { Component, OnDestroy } from '@angular/core';
+import { combineLatest, filter, map, Subscription } from 'rxjs';
+
+import { RacesFacade } from 'src/app/+state/races/races.facade';
+import { RacesEntity } from 'src/app/+state/races/races.models';
+import { SeasonsFacade } from 'src/app/+state/seasons/seasons.facade';
+import { SeasonsEntity } from 'src/app/+state/seasons/seasons.models';
 
 @Component({
   selector: 'rxf1-hero-devs-race-results-list',
   templateUrl: './race-results-list.component.html',
   styleUrls: ['./race-results-list.component.scss'],
 })
-export class RaceResultsListComponent {
-  @Input()
-  raceFinalResults: RacesFinalResult[] = [];
+export class RaceResultsListComponent implements OnDestroy {
+  raceFinalResults$ = this.racesFacade.raceFinalResults$;
+  selectedSeason$ = this.seasonsFacade.selectedSeason$;
+  selectedRace$ = this.racesFacade.selectedRace$;
 
   raceFinalResultsListTableColumns: string[] = [
     'position',
@@ -18,4 +24,35 @@ export class RaceResultsListComponent {
     'driverNationality',
     'time',
   ];
+
+  private selectedSeasonSub!: Subscription;
+
+  constructor(
+    private racesFacade: RacesFacade,
+    private seasonsFacade: SeasonsFacade
+  ) {
+    this.loadFinalResultsByRace();
+  }
+
+  ngOnDestroy(): void {
+    this.selectedSeasonSub.unsubscribe();
+  }
+
+  private loadFinalResultsByRace() {
+    this.selectedSeasonSub = combineLatest([
+      this.selectedSeason$.pipe(
+        filter((selectedSeason) => !!selectedSeason && !!selectedSeason.id),
+        map((selectedSeason) => selectedSeason as SeasonsEntity)
+      ),
+      this.selectedRace$.pipe(
+        filter((selectedRace) => !!selectedRace && !!selectedRace.id),
+        map((selectedRace) => selectedRace as RacesEntity)
+      ),
+    ]).subscribe(([selectedSeason, selectedRace]) => {
+      this.racesFacade.loadFinalResultsByRace(
+        selectedSeason.id,
+        selectedRace.id
+      );
+    });
+  }
 }
--- src/app/pages/races/races-page/races-page.component.ts
+++ src/app/pages/races/races-page/races-page.component.ts
@@ -1,54 +1,8 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { combineLatest, filter, map } from 'rxjs';
-
-import { RacesFacade } from 'src/app/+state/races/races.facade';
-import { RacesEntity } from 'src/app/+state/races/races.models';
-import { SeasonsFacade } from 'src/app/+state/seasons/seasons.facade';
-import { SeasonsEntity } from 'src/app/+state/seasons/seasons.models';
-import { RacesListComponent } from '../races-list/races-list.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'rxf1-hero-devs-races-page',
   templateUrl: './races-page.component.html',
   styleUrls: ['./races-page.component.scss'],
 })
-export class RacesPageComponent implements AfterViewInit {
-  selectedSeason$ = this.seasonsFacade.selectedSeason$;
-  raceFinalResults$ = this.racesFacade.raceFinalResults$;
-  selectedRace$ = this.racesFacade.selectedRace$;
-
-  @ViewChild(RacesListComponent, { static: true })
-  racesListComponent!: RacesListComponent;
-
-  constructor(
-    private racesFacade: RacesFacade,
-    private seasonsFacade: SeasonsFacade
-  ) {}
-
-  ngAfterViewInit(): void {
-    this.loadFinalResultsByRace();
-  }
-
-  markRaceAsSelect(race: RacesEntity) {
-    this.racesFacade.selectRace(race.id);
-  }
-
-  private loadFinalResultsByRace() {
-    combineLatest([
-      this.selectedSeason$.pipe(
-        filter((selectedSeason) => !!selectedSeason && !!selectedSeason.id),
-        map((selectedSeason) => selectedSeason as SeasonsEntity)
-      ),
-      this.selectedRace$.pipe(
-        filter((selectedRace) => !!selectedRace && !!selectedRace.id),
-        map((selectedRace) => selectedRace as RacesEntity)
-      ),
-    ]).subscribe(([selectedSeason, selectedRace]) => {
-      this.racesFacade.loadFinalResultsByRace(
-        selectedSeason.id,
-        selectedRace.id
-      );
-    });
-    // ? unsubscribe on destroy
-  }
-}
+export class RacesPageComponent {}
